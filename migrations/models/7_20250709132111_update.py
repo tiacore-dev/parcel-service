4519c4aa2323af748e9c533e3e2ef35903f6ee2e
Viktoria <v.s.polikanova@gmail.com>
2025-07-09 13:26:11 +0700

    дефолтное значение в миграции

--- migrations/models/7_20250709132111_update.py
+++ migrations/models/7_20250709132111_update.py
@@ -3,8 +3,9 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "parcels" ADD "recipient_delivery_type" VARCHAR(9)NOT NULL;
-        ALTER TABLE "parcels" ADD "sender_delivery_type" VARCHAR(9)NOT NULL;"""
+        ALTER TABLE "parcels" ADD "recipient_delivery_type" VARCHAR(9) NOT NULL DEFAULT 'door';
+        ALTER TABLE "parcels" ADD "sender_delivery_type" VARCHAR(9) NOT NULL DEFAULT 'door';
+    """
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
